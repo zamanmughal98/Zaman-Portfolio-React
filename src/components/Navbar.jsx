243968f53bf121252cded2fe2c21a06c1c2cb414
Zaman Mughal <zamanmughal98@gmail.com>
2024-09-14 20:36:53 +0500

    feat(Navbar): Refactor JSX and CSS for improved responsiveness and performance
    - Refactored JSX to reduce reliance on state for responsive styles.
    - Replaced useLayoutEffect with useEffect for better performance and applied debouncing for resize events.
    - Removed unnecessary state-based toggling for menu display, leveraging CSS media queries for handling screen sizes <= 768px.
    - Updated CSS for more efficient handling of mobile responsiveness:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,45 +4,54 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import { IoCloseCircleOutline } from 'react-icons/io5';
 
 import { NavLink } from 'react-router-dom';
-import { useState, useLayoutEffect } from 'react';
+import { useState, useEffect } from 'react';
 
+import { debounce } from '../config/utils';
 import ZLogo from './ZLogo';
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
   const [icons, setIcons] = useState('up');
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth); // State for window width
 
   const iconsMap = {
     up: GiHamburgerMenu,
     down: IoCloseCircleOutline,
   };
 
-  useLayoutEffect(() => {
-    const handleResize = () =>
-      window.innerWidth > 768 ? setShowMenu(true) : setShowMenu(false);
+  useEffect(() => {
+    // Debounced window resize handler in milliseconds
+    const handleResize = debounce(() => setWindowWidth(window.innerWidth), 300);
+
     window.addEventListener('resize', handleResize);
 
-    handleResize();
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  const toggleMenuState = () => {
-    setShowMenu(!showMenu);
-  };
+  useEffect(() => setShowMenu(windowWidth > 768), [windowWidth]);
+
+  const toggleMenuState = () => setShowMenu((prev) => !prev);
+
   const handleDropdownButton = (e) => {
-    setIcons((prev) => (prev === 'up' ? 'down' : 'up'));
     e.preventDefault();
+
+    setIcons((prev) => (prev === 'up' ? 'down' : 'up'));
+
     toggleMenuState();
   };
 
-  const handleNavLinkClass = ({ isActive }) => {
-    return isActive ? 'ActiveLinks' : 'unActiveLinks';
+  const handleNavLinkClass = ({ isActive }) =>
+    isActive ? 'ActiveLinks' : 'unActiveLinks';
+
+  const handleLinkClick = () => {
+    if (windowWidth <= 768) {
+      setShowMenu(false);
+      setIcons('up');
+    }
   };
 
   const IconComponent = iconsMap[icons];
+
   return (
     <header className="NavigationBarWrapper">
       <nav className="navContainer flexJustifySpace">
@@ -61,19 +70,34 @@ const Navbar = () => {
           className={`flexJustifySpace menuNavLinks ${
             showMenu ? 'show' : 'hidden'
           }`}>
-          <NavLink to="/" className={handleNavLinkClass}>
+          <NavLink
+            to="/"
+            className={handleNavLinkClass}
+            onClick={handleLinkClick}>
             Home
           </NavLink>
-          <NavLink to="/skills" className={handleNavLinkClass}>
+          <NavLink
+            to="/skills"
+            className={handleNavLinkClass}
+            onClick={handleLinkClick}>
             Skills
           </NavLink>
-          <NavLink to="/projects" className={handleNavLinkClass}>
+          <NavLink
+            to="/projects"
+            className={handleNavLinkClass}
+            onClick={handleLinkClick}>
             Projects
           </NavLink>
-          <NavLink to="resume" className={handleNavLinkClass}>
+          <NavLink
+            to="/resume"
+            className={handleNavLinkClass}
+            onClick={handleLinkClick}>
             Resume
           </NavLink>
-          <NavLink to="/contact" className={handleNavLinkClass}>
+          <NavLink
+            to="/contact"
+            className={handleNavLinkClass}
+            onClick={handleLinkClick}>
             Contact
           </NavLink>
         </section>
@@ -81,4 +105,5 @@ const Navbar = () => {
     </header>
   );
 };
+
 export default Navbar;
