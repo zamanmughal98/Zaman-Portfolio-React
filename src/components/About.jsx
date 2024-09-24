54a3be39786b039f93860ca984283522c84c2fb6
Zaman Mughal <zamanmughal98@gmail.com>
2024-09-24 20:14:02 +0500

    Added About section with full layout: - Created About.jsx for content and layout. - Designed About.css for styling and responsiveness, including font scaling, padding, and media queries. - Implemented responsive adjustments for mobile screens.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,15 @@
 import '../styles/About.css';
 
-import image from '../assets/images/Hero Image 1.png';
+import image1 from '../assets/images/Hero Image 1.png';
+import image3 from '../assets/images/Hero Image 3.png';
 
 const About = ({ showSectionTitle = false }) => {
+  const aboutmeImage = showSectionTitle ? image1 : image3;
+
   const myDetails = {
     name: 'Zaman Bin Ishfaq',
     DOB: 'December 24, 1998',
-    address: 'Islamabad',
+    address: 'Islamabad, Pakistan',
     email: 'zamanmughal98@gmail.com',
     phone: '+92 343 5598805',
   };
@@ -20,11 +23,11 @@ const About = ({ showSectionTitle = false }) => {
       )}
 
       <section className="aboutmeContentWrapper">
-        <div className="imageSideContainer">
-          <img className="image" src={image} alt="about-Image" />
+        <div className="aboutmeImageContainer">
+          <img className="aboutmeImage" src={aboutmeImage} alt="about-Image" />
         </div>
 
-        <div className="textSideContainer">
+        <div className="aboutmeTextContainer">
           <div className="introductorySentences">
             A dedicated MERN Stack Web Engineer with over two years of
             experience in designing and developing dynamic web applications.
@@ -32,27 +35,32 @@ const About = ({ showSectionTitle = false }) => {
             I specialize in creating robust, scalable, and user-friendly
             solutions.
           </div>
-          <section className="detailsWrapper">
-            <li>
-              <span className="keys"> Name:</span>
-              <span className="values">{myDetails.name}</span>
-            </li>
-            <li>
-              <span className="keys"> Date of birth:</span>
-              <span className="values">{myDetails.DOB}</span>
-            </li>
-            <li>
-              <span className="keys">Address:</span>
-              <span className="values">{myDetails.address}</span>
-            </li>
-            <li>
-              <span className="keys">Email:</span>
-              <span className="values">{myDetails.email}</span>
-            </li>
-            <li>
-              <span className="keys"> Phone:</span>
-              <span className="values">{myDetails.phone}</span>
-            </li>
+
+          <section className="detailsLists">
+            <div className="detailsLists_Item">
+              <span className="detailsList_key"> Name:</span>
+              <span className="detailsList_value">{myDetails.name}</span>
+            </div>
+
+            <div className="detailsLists_Item">
+              <span className="detailsList_key"> Date of birth:</span>
+              <span className="detailsList_value">{myDetails.DOB}</span>
+            </div>
+
+            <div className="detailsLists_Item">
+              <span className="detailsList_key">Address:</span>
+              <span className="detailsList_value">{myDetails.address}</span>
+            </div>
+
+            <div className="detailsLists_Item">
+              <span className="detailsList_key">Email:</span>
+              <span className="detailsList_value">{myDetails.email}</span>
+            </div>
+
+            <div className="detailsLists_Item">
+              <span className="detailsList_key"> Phone:</span>
+              <span className="detailsList_value">{myDetails.phone}</span>
+            </div>
           </section>
         </div>
       </section>
