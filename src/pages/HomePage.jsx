0e77950fad9536a6b5b19909c6b74215e60f31f7
Zaman Mughal <zamanmughal98@gmail.com>
2024-09-26 18:45:47 +0500

    refactor(HomePage) - passed the data of slider to the Hero component - passed the data of introduction sentence and other details to the About component
    refactor(SkillsPage)
    - passed the data of introduction sentence and other details to the About component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,49 @@ import '../styles/HomePage.css';
 
 import Hero from '../components/Hero';
 import About from '../components/About';
+import HeadingwithShadow from '../components/HeadingwithShadow';
 
 const HomePage = () => {
+  const slides = [
+    {
+      greeting: 'Hello',
+      introPrefix: "I'm",
+      mainText: 'Zaman Bin Ishfaq',
+      additionalInfo: 'MERN Stack Web Engineer',
+    },
+    {
+      greeting: 'Hello',
+      introPrefix: "I'm a",
+      mainText: 'Web Engineer',
+      additionalInfo: 'based in Islamabad',
+    },
+    {
+      greeting: 'Hi',
+      introPrefix: "I'm",
+      mainText: 'A Passionate Developer',
+      additionalInfo: 'With Experience in Building Scalable Web Applications',
+    },
+  ];
+  const introductorySentences =
+    ' A dedicated MERN Stack Web Engineer with over two years of experience in designing and developing dynamic web applications. With a strong foundation in both frontend and backend technologies, I specialize in creating robust, scalable, and user-friendly solutions.';
+
+  const myDetails = [
+    { label: 'Name', content: 'Zaman Bin Ishfaq' },
+    { label: 'DOB', content: 'December 24, 1998' },
+    { label: 'Address', content: 'Islamabad, Pakistan' },
+    { label: 'Email', content: 'zamanmughal98@gmail.com' },
+    { label: 'Phone', content: '+92 343 5598805' },
+  ];
+
   return (
     <section className="homePageContainer">
-      <Hero />
-      <About showSectionTitle />
+      <Hero sliderData={slides} />
+      <HeadingwithShadow title={'about Me'} shadow={'About'} />
+      <About
+        showSectionTitle
+        introductorySentences={introductorySentences}
+        details={myDetails}
+      />
     </section>
   );
 };
--- src/pages/SkillsPage.jsx
+++ src/pages/SkillsPage.jsx
@@ -1,11 +1,24 @@
-import '../styles/SkillsPage.css'
+import '../styles/SkillsPage.css';
 
 import About from '../components/About';
 
 const HomePage = () => {
+  const introductorySentences =
+    ' A dedicated MERN Stack Web Engineer with over two years of experience in designing and developing dynamic web applications. With a strong foundation in both frontend and backend technologies, I specialize in creating robust, scalable, and user-friendly solutions.';
+
+  const myDetails = [
+    { label: 'Name', content: 'Zaman Bin Ishfaq' },
+    { label: 'DOB', content: 'December 24, 1998' },
+    { label: 'Address', content: 'Islamabad, Pakistan' },
+    { label: 'Email', content: 'zamanmughal98@gmail.com' },
+    { label: 'Phone', content: '+92 343 5598805' },
+  ];
   return (
     <section className="skillsPageContainer">
-      <About />
+      <About
+        introductorySentences={introductorySentences}
+        details={myDetails}
+      />
     </section>
   );
 };
