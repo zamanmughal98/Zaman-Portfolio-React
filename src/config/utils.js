ce730d69d2cce4dbc4412032f261e4eb8d5a6867
Zaman Mughal <zamanmughal98@gmail.com>
2024-09-26 18:42:57 +0500

    feat(capitalizeSentence) created a method in utils to capitalize sentence first letters and used it on multiple places

--- src/config/utils.js
+++ src/config/utils.js
@@ -11,3 +11,13 @@ export const debounce = (callback, delay) => {
     }, delay);
   };
 };
+
+export const capitalizeSentence = (message = '') => {
+  if (message && (typeof message !== 'string' || !message.trim()))
+    return message;
+
+  return message
+    .split(' ')
+    ?.map((part) => part.charAt(0).toUpperCase() + part.slice(1))
+    ?.join(' ');
+};
