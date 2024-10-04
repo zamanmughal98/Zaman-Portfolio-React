e89a10e4af5b3898f1a8304711a14014ff55cc1f
Zaman Mughal <zamanmughal98@gmail.com>
2024-10-04 22:20:55 +0500

    feat: created the myTools Component and render the list of the Tools

---
+++ src/components/skillSet/MyTools.jsx
@@ -0,0 +1,9 @@
+import { technologiesList } from './Skills.iconsData';
+
+import SkillsListing from './ItemListing';
+
+const MyTools = () => {
+  return <SkillsListing List={technologiesList} />;
+};
+
+export default MyTools;
