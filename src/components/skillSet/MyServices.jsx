4885bb8d10cc542fcbc4a654ae02afcced5249ae
Zaman Mughal <zamanmughal98@gmail.com>
2024-10-08 23:59:24 +0500

    refactor: Implement service list visibility and screen resize handling using two useEffects. Added logic to manage "See More" button visibility and dynamically load more services on click. Applied debounce to optimize window resize event listener.

--- src/components/skillSet/MyServices.jsx
+++ src/components/skillSet/MyServices.jsx
@@ -1,9 +1,54 @@
 import '../../styles/MyServices.css';
 
+import { MdUnfoldMoreDouble } from 'react-icons/md';
+
+import { useState, useEffect } from 'react';
+
 import { servicesList, iconMapping } from './IconsData';
-import { capitalizeSentence } from '../../config/utils.js';
+import { capitalizeSentence, debounce } from '../../config/utils.js';
 
 const MyServices = () => {
+  const [visibleItems, setVisibleItems] = useState(4);
+  const [isVisible, setIsVisible] = useState(true);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const seeMoreHandler = () => {
+    const remainingItems = servicesList.length - visibleItems;
+
+    if (remainingItems <= 4) {
+      setVisibleItems(servicesList.length);
+      setIsVisible(false);
+    } else {
+      setVisibleItems(visibleItems + 4);
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = debounce(() => {
+      setWindowWidth(window.innerWidth);
+    }, 300);
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (windowWidth > 768) {
+      setVisibleItems(servicesList.length);
+      setIsVisible(false);
+    } else {
+      setVisibleItems(4);
+      setIsVisible(servicesList.length > 4);
+    }
+  }, [windowWidth]);
+
+  const seeMoreButton = {
+    title: 'see more',
+    onclickHandler: seeMoreHandler,
+    icon: <MdUnfoldMoreDouble className="seeMoreServiesButtonIcon" />,
+  };
+
   return (
     <section className="myServicesContainer">
       <div className="serviceCardContainer">
