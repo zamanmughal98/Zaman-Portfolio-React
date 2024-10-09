c47d1bed2b59326cbef4badbaf1f29e030cfa78b
Zaman Mughal <zamanmughal98@gmail.com>
2024-10-09 15:57:41 +0500

    refactor: added the logic of see more from the UseSeeMore hook into the my tools component

--- src/components/skillSet/MyTools.jsx
+++ src/components/skillSet/MyTools.jsx
@@ -1,9 +1,45 @@
+import '../../styles/MySkills.css';
+
+import { MdUnfoldMore } from 'react-icons/md';
+
 import { technologiesList } from './IconsData';
+
+import useSeeMore from '../../hooks/useSeeMore.js';
+
+import { capitalizeSentence } from '../../config/utils.js';
 
 import ItemListing from './ItemListing';
 
 const MyTools = () => {
-  return <ItemListing List={technologiesList} />;
+  const nextIncrementof = 5;
+
+  const {
+    visibleItems,
+    isVisible: isButtonVisible,
+    seeMoreHandler,
+  } = useSeeMore(technologiesList, nextIncrementof);
+
+  const seeMoreButton = {
+    title: 'see more tools',
+    onclickHandler: seeMoreHandler,
+    icon: <MdUnfoldMore className="seeMoreSkillButtonIcon" />,
+  };
+  return (
+    <>
+      <ItemListing List={technologiesList.slice(0, visibleItems)} />
+
+      <br />
+
+      {isButtonVisible && (
+        <button
+          className="seeMoreServiesButton"
+          onClick={seeMoreButton.onclickHandler}>
+          {capitalizeSentence(seeMoreButton.title)}
+          {seeMoreButton.icon}
+        </button>
+      )}
+    </>
+  );
 };
 
 export default MyTools;
