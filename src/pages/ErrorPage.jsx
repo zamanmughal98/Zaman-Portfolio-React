7cfc1e625464475ea9bc12d4e65a1b695ba77f77
Zaman Mughal <zamanmughal98@gmail.com>
2024-10-15 15:19:36 +0500

    refactor: clean up the error page, move the code into the separate component

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,24 +1,12 @@
-import '../styles/404ErrorPage.css';
-
-import errorImage from '../assets/images/404 error.png';
-
-import { Link } from 'react-router-dom';
-import { capitalizeSentence } from '../config/utils';
-
-const ErrorPage = ({ errorDetails }) => {
-  const { errorHeading, errorMessage, gotoRoute, linkto } = errorDetails;
-  return (
-    <section className="errorPageWrapper">
-      <img src={errorImage} className="errorImage" />
-      <h1 className="errorPageTitles">{errorHeading}</h1>
-      <div className="flexRow">
-        <div>{errorMessage}</div>
-        <Link className="linkSubTitle" to={linkto}>
-          {capitalizeSentence(gotoRoute)}
-        </Link>
-      </div>
-    </section>
-  );
+import Error404 from '../components/Error404';
+const ErrorPage = () => {
+  const error = {
+    errorHeading: "Looks like you're lost",
+    errorMessage: 'Please check your URL or return to the',
+    gotoRoute: 'home',
+    linkto: '/',
+  };
+  return <Error404 errorDetails={error} />;
 };
 
 export default ErrorPage;
