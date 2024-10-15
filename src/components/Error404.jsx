f9b08076891fab32d100d3f30cb452f6de8a223f
Zaman Mughal <zamanmughal98@gmail.com>
2024-10-15 15:17:03 +0500

    feat: created the saperate files for Error 404 conponent

---
+++ src/components/Error404.jsx
@@ -0,0 +1,24 @@
+import '../styles/Error404.css';
+
+import errorImage from '../assets/images/404 error.png';
+
+import { Link } from 'react-router-dom';
+import { capitalizeSentence } from '../config/utils';
+
+const ErrorPage = ({ errorDetails }) => {
+  const { errorHeading, errorMessage, gotoRoute, linkto } = errorDetails;
+  return (
+    <section className="errorPageWrapper">
+      <img src={errorImage} className="errorImage" />
+      <h1 className="errorPageTitles">{errorHeading}</h1>
+      <div className="flexRow">
+        <div>{errorMessage}</div>
+        <Link className="linkSubTitle" to={linkto}>
+          {capitalizeSentence(gotoRoute)}
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default ErrorPage;
