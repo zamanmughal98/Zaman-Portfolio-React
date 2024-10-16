64c7b82b9942f835d8e99cd560ed225dae050504
Zaman Mughal <zamanmughal98@gmail.com>
2024-10-16 18:40:21 +0500

    added the heading and the shadow for the component

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,10 +1,13 @@
 import '../styles/pages/ContactPage.css';
 
+import HeadingwithShadow from '../components/HeadingwithShadow';
 import ContactMe from '../components/contact/ContactMe';
 
 const ContactPage = () => {
   return (
     <section className="contactPageContainer">
+      <HeadingwithShadow title="Contact Us" shadow="Contact" />
+      <br />
       <ContactMe />
     </section>
   );
