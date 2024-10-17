1a3bfa5ad4946d2d79f5bf5975c1d3fc719a8c6e
Zaman Mughal <zamanmughal98@gmail.com>
2024-10-17 12:53:22 +0500

    createda data file for contacts to handle other functions and objects

---
+++ src/components/contact/contactData.js
@@ -0,0 +1,62 @@
+export const successToaster = {
+  render: 'Message sent successfully!',
+  type: 'success',
+  isLoading: false,
+  autoClose: 5000,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  closeButton: true,
+};
+
+export const errorToaster = {
+  ...successToaster,
+  render: 'Message failed to send. Try again.',
+  type: 'error',
+};
+
+const validateName = (name) => {
+  if (name.trim() === '') {
+    return 'Name is required';
+  } else if (name.trim().length < 5) {
+    return 'Name must be at least 5 characters long';
+  }
+  return '';
+};
+
+const validateEmail = (email) => {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (email.trim() === '') {
+    return 'Email is required';
+  } else if (!emailPattern.test(email)) {
+    return 'Invalid email address';
+  }
+  return '';
+};
+
+const validateMessage = (message) => {
+  if (message.trim() === '') {
+    return 'Message is required';
+  } else if (message.trim().length < 10) {
+    return 'Message must be at least 10 characters long';
+  }
+  return '';
+};
+
+export const validationRules = {
+  senderName: validateName,
+  senderEmail: validateEmail,
+  senderMessage: validateMessage,
+};
+
+// For the testing of email and toasts
+
+// const toastId = toast.loading('Sending message...');
+// setIsSubmitting(true);
+
+// setTimeout(() => {
+//   toast.update(toastId, successToaster);
+//   // toast.update(toastId, errorToaster);
+
+//   setIsSubmitting(false);
+// }, 1000);
