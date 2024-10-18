ec798991f73988b84dce4c16b99187a427f0db9c
Zaman Mughal <zamanmughal98@gmail.com>
2024-10-18 21:17:18 +0500

    -remove the comtactMe component, and shifted its content to contactPage - move the contactMe style file in to the component folder and renamed it to contact.css

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,14 +1,29 @@
 import '../styles/pages/ContactPage.css';
+import '../styles/components/Contact.css';
+
+import contactmeImage from '../assets/images/Hero Image 3.png';
 
 import HeadingwithShadow from '../components/HeadingwithShadow';
-import ContactMe from '../components/contact/ContactMe';
+import ContactMeForm from '../components/contact/ContactMeForm';
 
 const ContactPage = () => {
   return (
     <section className="contactPageContainer">
       <HeadingwithShadow title="Contact Us" shadow="Contact" />
+
       <br />
-      <ContactMe />
+
+      <section className="contactMeWrapper">
+        <div className="contactmeImageContainer">
+          <img
+            className="contactmeImage"
+            src={contactmeImage}
+            alt="Contact-Image"
+          />
+        </div>
+
+        <ContactMeForm />
+      </section>
     </section>
   );
 };
