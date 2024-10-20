8ee2787c897ab76bb0bb19787dbe37a679df0d76
Zaman Mughal <zamanmughal98@gmail.com>
2024-10-20 21:43:35 +0500

    feat: created a project page with the placeholder text, shadow and title component

---
+++ src/pages/ProjectPage.jsx
@@ -0,0 +1,16 @@
+import '../styles/pages/ProjectPage.css';
+
+import HeadingwithShadow from '../components/common/HeadingwithShadow';
+
+const ProjectPage = () => {
+  return (
+    <section className="projectPageContainer">
+      <HeadingwithShadow title="My Recent Work" shadow="Projects" />
+      <br />
+
+      <div>Project page is loading </div>
+    </section>
+  );
+};
+
+export default ProjectPage;
